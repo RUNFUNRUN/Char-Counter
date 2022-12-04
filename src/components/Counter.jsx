5fea48f58b6e3a41531e9e1733c00c4820c5cfa0
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2022-12-05 02:20:00 +0900

    文字数カウント機能完成

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import '../App.css';
+import React, {useState, useEffect} from 'react';
+
+export function Counter() {
+    const [text, setText] = useState('');
+    const [count, setCount] = useState(text.length);
+    const [noNewlineCount, setNoNewlineCount] = useState(text.length);
+    const [simpleCount, setSimpleCount] = useState(text.length);
+
+    const handleCountChange = (e) => {
+        setText(e.target.value);
+    };
+
+    useEffect(() => {
+        setCount(text.length);
+        setNoNewlineCount(text.replace(/\r?\n/g, '').length);
+        setSimpleCount(text.replace(/\r?\n/g, '').replace(/\s/g, '').replace(/[、。,.]/g, '').length);
+    }, [text]);
+
+    return (
+        <div>
+            <div className="display">
+                <p>現在の文字数 : {count}</p>
+                <p>改行を除いた文字数 : {noNewlineCount}</p>
+                <p>改行・句読点・空白を除いた文字数 : {simpleCount}</p>
+            </div>
+            <textarea
+                rows="20" cols="80" value={text}
+                onChange={handleCountChange}
+            />
+        </div>
+    );
+}
---
+++ src/components/Head.jsx
@@ -0,0 +1,7 @@
+import '../App.css';
+
+export function Head() {
+    return (
+        <h1>文字数カウンター</h1>
+    );
+}
