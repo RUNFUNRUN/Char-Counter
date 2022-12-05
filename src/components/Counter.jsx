57d13aa83251e3d5c2b724a6bdc449d03fbde3dd
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2022-12-05 18:19:24 +0900

    句読点を記号に変更

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -14,7 +14,8 @@ export function Counter() {
     useEffect(() => {
         setCount(text.length);
         setNoNewlineCount(text.replace(/\r?\n/g, '').length);
-        setSimpleCount(text.replace(/\r?\n/g, '').replace(/\s/g, '').replace(/[、。,.]/g, '').length);
+        setSimpleCount(text.replace(/\r?\n/g, '').replace(/\s/g, '')
+            .replace(/[　、。！？「」『』（）；： ,.!?\[\]{}/();:]/g, '').length);
     }, [text]);
 
     return (
@@ -22,7 +23,7 @@ export function Counter() {
             <div className="display">
                 <p>現在の文字数 : {count}</p>
                 <p>改行を除いた文字数 : {noNewlineCount}</p>
-                <p>改行・句読点・空白を除いた文字数 : {simpleCount}</p>
+                <p>改行・記号・空白を除いた文字数 : {simpleCount}</p>
             </div>
             <textarea
                 rows="20" cols="80" value={text}
